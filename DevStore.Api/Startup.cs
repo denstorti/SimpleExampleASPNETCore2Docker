e08dce2ba61e552e869f15b93b3b74b562f3c531
Denis Storti <denstorti@hotmail.com>
2018-06-09 19:30:25 +1200

    Adding CORS Adding Swagger Switching envs and using memory or MSSQL DBs

--- DevStore.Api/Startup.cs
+++ DevStore.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DevStore.Api
 {
@@ -26,22 +27,45 @@ namespace DevStore.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connString = Configuration["ConnectionString"];
-            
-            services.AddDbContext<DevStoreDataContext>(optionsAction =>
-                optionsAction.UseSqlServer(connString,
-                
-                    sqlServerOptionsAction: sqlOptions =>
-                    {
-                        sqlOptions.MigrationsAssembly(typeof(DevStoreDataContext).GetTypeInfo().Assembly.GetName().Name);
+            Console.WriteLine("ASPNETCORE_ENVIRONMENT = " + Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != EnvironmentName.Development)
+            {
+                string connString = Configuration["ConnectionString"];
+
+                services.AddDbContext<DevStoreDataContext>(optionsAction =>
+                    optionsAction.UseSqlServer(connString,
+
+                        sqlServerOptionsAction: sqlOptions =>
+                        {
+                            sqlOptions.MigrationsAssembly(typeof(DevStoreDataContext).GetTypeInfo().Assembly.GetName().Name);
 
                         //Configuring Connection Resiliency: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency 
                         sqlOptions.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
-                    }
-                )
-            );
-            
+                        }
+                    )
+                );
+
+                Console.WriteLine("DATABASE: Using SQL Server");
+            }
+            else
+            {
+                Console.WriteLine("DATABASE: Using In Memory database");
+                
+                services.AddDbContext<DevStoreDataContext>(optionsAction =>
+                    optionsAction.UseInMemoryDatabase("InMemory")
+                );
+            }
+
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Simple ASP.NET Core 2 + SQL Server on Docker", Version = "v1" });
+            });
+
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,18 +75,28 @@ namespace DevStore.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    var context = serviceScope.ServiceProvider.GetService<DevStoreDataContext>();
 
-			using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
-			{
-				var context = serviceScope.ServiceProvider.GetService <DevStoreDataContext> ();
-					
-				context.Database.Migrate();
-					
-			}
-            
+                    context.Database.Migrate();
+                }
+
+            }
+
             app.UseMvc();
-            
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+            );
         }
     }
 }
